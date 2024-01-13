6b1a37d5c0c3ddd79ac2dfb6d8f40fae39672586
Juliya Shamray <fizmatka908990@gmail.com>
2024-01-13 22:14:52 +0200

    common setting/styles

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,56 @@
+const { createGlobalStyle } = require('styled-components');
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+   font-family: "Manrope",sans-serif;
+   margin: 0 auto;
+   font-size: 16px;
+   font-style: normal;
+   font-weight: 400;
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+ul,
+ol {
+  margin: 0;
+  padding: 0;
+}
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+a{
+  text-decoration: none;
+  color: inherit;
+}
+  }
+  .container {
+    
+    padding-left: 15px;
+    padding-right: 15px;
+    margin-left: auto;
+    margin-right: auto;
+    @media screen and (min-width:320px) {
+        max-width: 375px;
+}
+    @media screen and (min-width:768px) {
+        max-width: 768px;
+}
+
+    @media screen and (min-width:1240px) {
+        max-width: 1240px;
+    
+}
+
+
+}
+
+`;
